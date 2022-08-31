97bc40262562d4dc951fe2b2bdf2489ab66559b2
Sebastian Aguilera <inforaguilera@gmail.com>
2022-08-30 20:01:11 -0400

    ejercicio de gridd y js (muy basico)

---
+++ script.js
@@ -0,0 +1,50 @@
+
+
+let numero = document.querySelector('.numero');
+let num1 = document.querySelector('.box1');
+let num2 = document.querySelector('.box2');
+let num3 = document.querySelector('.box3');
+let num4 = document.querySelector('.box4');
+let num5 = document.querySelector('.box5');
+let num6 = document.querySelector('.box6');
+let num7 = document.querySelector('.box7');
+let num8 = document.querySelector('.box8');
+let num9 = document.querySelector('.box9');
+
+
+num1.addEventListener('click', () => {
+    numero.textContent = 1;
+});
+
+num2.addEventListener('click', () => {
+    numero.textContent = 2;
+});
+
+num3.addEventListener('click', () => {
+    numero.textContent = 3;
+});
+
+num4.addEventListener('click', () => {
+    numero.textContent = 4;
+});
+
+num5.addEventListener('click', () => {
+    numero.textContent = 5;
+});
+
+num6.addEventListener('click', () => {
+    numero.textContent = 6;
+});
+
+num7.addEventListener('click', () => {
+    numero.textContent = 7;
+});
+
+num8.addEventListener('click', () => {
+    numero.textContent = 8;
+});
+
+num9.addEventListener('click', () => {
+    numero.textContent = 9;
+});
+
